7a8c88183206b20fd2163b82685838f2ec873854
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-06-06 18:16:20 +0530

    Path Parameters in REST APIs

--- src/main/java/com/harsh/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/harsh/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,8 +1,9 @@
 package com.harsh.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 //Rest API
@@ -28,5 +29,16 @@ public class HelloWorldController
     public HelloWorldBean helloWorldBean()
     {
         return new HelloWorldBean("Hello World");
+    }
+
+    //Path parameters
+    // /user/{id}/todos/{id} -> /users/2/todos/200
+    // /hello-world/path-variable/{name}
+    // /hello-world/path-variable/harsh
+
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) 
+    {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
