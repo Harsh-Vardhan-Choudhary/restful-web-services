6833856f89daa7654533eef0840cc387a3147503
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-06-08 03:10:20 +0530

    Internationalization (i18n) in REST APIs

--- src/main/java/com/harsh/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/harsh/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.harsh.rest.webservices.restful_web_services.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController
 {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource)
+    {
+        this.messageSource = messageSource;
+    }   
+
     // similarly we can use postmapping and another annotations for mapping urls
     @GetMapping(path = "/hello-world")
     public String helloWorld()
@@ -41,4 +53,31 @@ public class HelloWorldController
     {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
+
+    // @GetMapping("/hello-world-internationalized")
+    // public  String helloWorldInternationalized()
+    // {
+    //     Locale locale = LocaleContextHolder.getLocale();
+    //     messageSource.getMessage("good.morning.message", null, "Default Message", locale);   
+
+    //     return "hello world";
+    // }
+
+    @GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() 
+    {
+		Locale locale = LocaleContextHolder.getLocale();
+
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale );
+		
+		//return "Hello World V2"; 
+		
+//		- Example: `en` - English (Good Morning)
+//		- Example: `nl` - Dutch (Goedemorgen)
+//		- Example: `fr` - French (Bonjour)
+//		- Example: `de` - Deutsch (Guten Morgen)
+
+	}
+
+   
 }
